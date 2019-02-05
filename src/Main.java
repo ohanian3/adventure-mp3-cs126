b95039d9a8f978d88f9853349ce4e1f51a08ba63
jackson ohanian <ohanian3@illinois.edu>
2019-02-05 11:32:47 -0600

    Since I already had some this is gonna be a big jump, but basically just made json reader, and implemented a hierarchy of classes to handle all the different types of objects coming from json.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class Main {
 
+    // json file in String format
+    private static String jsonString = "";
+    // User input scanner
+    private static Scanner user = new Scanner(System.in);
+    // The map as found from Json
+    private static Maps map;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        getInfo();
+        Gson gson = new Gson();
+        map = gson.fromJson(jsonString, Maps.class);
+        System.out.println("Start Game? Y/N ");
+        if (user.nextLine().toLowerCase().equals("y")) {
+            System.out.println("ok buddy");
+            gameplay();
+        } else {
+            System.out.println("ok");
+        }
+
+
+    }
+
+    /**
+     * gameplay handling user input and system output
+     */
+    private static void gameplay() {
+        System.out.println("You start in " + map.getStartingRoom());
+    }
+
+    private static void getInfo() {
+        try {
+            // This code is from the lecture notes
+            URL url = new URL("https://courses.engr.illinois.edu/cs126/adventure/siebel.json");
+            InputStream inStream = url.openStream();
+            jsonString = convertStreamToString(inStream);
+            System.out.println(jsonString);
+
+        } catch (Exception MalformedURLException) {
+            System.out.println("Malformed URL Error");
+        }
+
+
+
+    }
+
+    // Peter Mortennson
+    // https://stackoverflow.com/questions/309424
+    private static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
+
